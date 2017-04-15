97992331fb67334ffc060da6589f9b5c0e1d39e7
Adam Nickle <anickle060193@gmail.com>
2017-04-15 18:50:11 +0000

    Shortened URLs for restaurants.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [ :show ]
 
-  resources :restaurants, only: [ :index, :show, :new, :create ]
+  get '/search', to: "restaurants#index", as: :restaurants
+  post '/create', to: "restaurants#create", as: :create_restaurant
+  get '/new', to: "restaurants#new", as: :new_restaurant
+  get '/create' => redirect( '/new' )
+  get '/:id', to: "restaurants#show", as: :restaurant
 
   resources :restaurant_likes, only: [ :create, :destroy ]
 end
