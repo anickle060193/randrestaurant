6b8b7741df91fdb862870f01f36067966396da18
Adam Nickle <anickle060193@gmail.com>
2017-04-19 02:46:17 +0000

    Add email support using Sendgrid.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,16 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'https://rand.restaurant' }
+  config.action_mailer.default_url_options = { host: 'rand.restaurant', protocol: 'https' }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV[ 'SENDGRID_USERNAME' ],
+    :password       => ENV[ 'SENDGRID_PASSWORD' ],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
