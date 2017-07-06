870458005510e7f7514a5c9f07ebe8643e918e99
Adam Nickle <anickle060193@gmail.com>
2017-07-06 17:46:32 -0500

    Fix database migration failing due to foreign key constraints.

--- db/migrate/20170703020535_change_restaurants_to_use_google_places.rb
+++ db/migrate/20170703020535_change_restaurants_to_use_google_places.rb
@@ -1,6 +1,9 @@
 class ChangeRestaurantsToUseGooglePlaces < ActiveRecord::Migration[5.0]
   def change
     reversible do |change|
+      execute 'DELETE FROM restaurant_likes'
+      execute 'DELETE FROM never_agains'
+      execute 'DELETE FROM meals'
       execute 'DELETE FROM restaurants'
     end
 
