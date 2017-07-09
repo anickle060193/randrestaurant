2691d3b88980a41afff04761698d3b47b4639361
Adam Nickle <anickle060193@gmail.com>
2017-07-08 21:36:28 -0500

    Protected PossibleMealRestaurants from being added/removed by non-Meal organizer.

--- app/controllers/possible_meal_restaurants_controller.rb
+++ app/controllers/possible_meal_restaurants_controller.rb
@@ -1,10 +1,12 @@
 class PossibleMealRestaurantsController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_models
+  before_action :correct_user
 
   def create
     @meal = Meal.find( params[ :meal_id ] )
-    @added_possible_restaurant = Restaurant.from_place_id( params[ :place_id ] )
-    @meal.possible_restaurants << @added_possible_restaurant unless @meal.possible_restaurants.exists?( @added_possible_restaurant.id )
+    @possible_restaurant = Restaurant.from_place_id( params[ :place_id ] )
+    @meal.possible_restaurants << @possible_restaurant unless @meal.possible_restaurants.exists?( @possible_restaurant.id )
 
     respond_to do |format|
       format.html { redirect_to @meal }
@@ -16,21 +18,15 @@ class PossibleMealRestaurantsController < ApplicationController
     possible_meal_restaurant = PossibleMealRestaurant.find_by( id: params[ :id ] )
     if possible_meal_restaurant.present?
       PossibleMealRestaurant.transaction do
-        @meal = possible_meal_restaurant.meal
-        @removed_possible_restaurant = possible_meal_restaurant.restaurant
-
         possible_meal_restaurant.destroy!
 
-        if @removed_possible_restaurant == @meal.restaurant
+        if @possible_restaurant == @meal.restaurant
           @meal.restaurant = nil
           @meal.save!
           redirect_to @meal
           return
         end
       end
-    else
-      @meal = Meal.find( params[ :meal_id ] )
-      @removed_possible_restaurant = Restaurant.find( params[ :restaurant_id ] )
     end
 
     respond_to do |format|
@@ -38,5 +34,18 @@ class PossibleMealRestaurantsController < ApplicationController
       format.js
     end
   end
+
+  private
+
+    def find_models
+      @meal = Meal.find( params[ :meal_id ] )
+      @possible_restaurant = Restaurant.from_place_id( params[ :place_id ] )
+    end
+
+    def correct_user
+      unless @meal.organizer == current_user
+        redirect_to @meal
+      end
+    end
 
 end
